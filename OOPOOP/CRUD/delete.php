acc82b4a47d6dde15f64b50397fa49d499a10d64
Joseph Wambua <101257432+josefwambua@users.noreply.github.com>
2024-09-17 20:30:32 +0300

    Update delete.php

--- OOPOOP/CRUD/delete.php
+++ OOPOOP/CRUD/delete.php
@@ -2,12 +2,13 @@
 include 'config.php';
 
 if (isset($_GET['id'])) {
+    // getting user by id
     $user_id = $_GET['id'];
 
-    
+    // deleting from the database
     $sql = "DELETE FROM users WHERE id='$user_id'";
     $result = $conn->query($sql);
-
+ // outcome whether true or false
     if ($result === TRUE) {
         echo "User deleted successfully";
     } else {
